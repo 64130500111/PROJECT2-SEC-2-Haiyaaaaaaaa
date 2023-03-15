978f56651e057c3c1fed091e1df5394d245593af
64130500072_wathanyu <89213510+WathanyuChomsri@users.noreply.github.com>
2023-03-16 01:31:43 +0700

    Add files via upload

---
+++ name-project/src/components/MenuShowing.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+    menu: {
+        type: Object,
+        require: true
+    }
+})
+</script>
+ 
+<template>
+    <div class="flex justify-center">
+        <img :src="menu.picURL" class="w-[120px] h-[120px] rounded-[30px]" />
+    </div>
+
+    <div class="flex flex-row justify-center">
+        <p class="font-['Baloo'] text-[0.9em] text-center mt-3">
+            {{ menu.name }}
+        </p>
+    </div>
+
+    <div class="flex flex-row">
+              <div class="w-full flex justify-center mt-7">
+     <p class="font-['Baloo'] text-lg" >{{ menu.price }}฿</p>
+    </div>
+
+    </div>
+
+    
+
+</template>
+ 
+<style scoped></style>
---
+++ name-project/src/components/SearchMenu.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+const props = defineProps({
+    search: {
+        type: String ,
+        require: true
+    },
+    index: {
+    type: Number,
+    require: true
+  }
+})
+
+defineEmits(['searchMenu'])
+</script>
+
+<template>
+    <div class="w-full flex justify-end">
+                <div class="w-[45px] h-[45px] flex flex-row mt-3 bg-gray-200 rounded-xl shadow-lg">
+                  <!-- <button @click="search = !search" class="mt-[5px] ml-[10px]">
+                    <MaterialSymbolsSearchRounded />
+                  </button> -->
+                </div>
+                <input @input="$emit('searchMenu',search,index)"
+                v-model.trim="userKeywords" type="text"
+                  placeholder="Type keyword..."
+                  class="w-[10em] h-8 rounded-md ml-3 mt-5 p-2 bg-white border border-gray-500" />
+              </div>
+
+</template>
